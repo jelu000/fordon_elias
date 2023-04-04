0291cffd555d8f72fdc62b982de0732d6cc4cdfe
Jens Lundeqvist <jens.lundeqvist@storsjogymnasiet.se>
2023-04-04 10:13:10 +0200

    Klar med Fordon

---
+++ fordon.py
@@ -0,0 +1,21 @@
+#detta är en basklass
+class Fordon:
+
+    #constructor
+    def __init__(self, fabrikat, color):
+        self.fabrikat = fabrikat
+        self.color = color
+
+        #funktioner som tillhör en klass kallas för metoder
+
+    def set_fabrikat(self, fabrikat):
+        self.fabrikat = fabrikat
+
+    def get_fabrikat(self):
+        return self.fabrikat
+            
+    def set_color(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
